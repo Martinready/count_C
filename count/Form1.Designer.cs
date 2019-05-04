6925321a7d27c817905d975229d05950887b2c95
Martinready <errmartin3@gmail.com>
2019-05-04 21:04:12 +0800

    first commit

---
+++ count/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace count
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "存檔";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(225, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "支出";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "收入";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(35, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(201, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(225, 22);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 15;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(401, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "現金";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(552, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "飲食";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(552, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 16);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "衣服";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(554, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 16);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "住";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(552, 151);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 16);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "交通";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(401, 132);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 16);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "總花費";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label9.Location = new System.Drawing.Point(401, 177);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 16);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "剩餘";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label10.Location = new System.Drawing.Point(478, 177);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(16, 16);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label11.Location = new System.Drawing.Point(478, 132);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(16, 16);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label12.Location = new System.Drawing.Point(478, 28);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(16, 16);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "0";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label13.Location = new System.Drawing.Point(599, 26);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(16, 16);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "0";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label14.Location = new System.Drawing.Point(599, 65);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(16, 16);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "0";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label15.Location = new System.Drawing.Point(599, 107);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(16, 16);
+            this.label15.TabIndex = 28;
+            this.label15.Text = "0";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label16.Location = new System.Drawing.Point(599, 150);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(16, 16);
+            this.label16.TabIndex = 29;
+            this.label16.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(271, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 30;
+            this.button3.Text = "確認";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label17.Location = new System.Drawing.Point(478, 87);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(16, 16);
+            this.label17.TabIndex = 31;
+            this.label17.Text = "0";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(401, 87);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(72, 16);
+            this.label18.TabIndex = 32;
+            this.label18.Text = "本次花費";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+    }
+}
+
---
+++ count/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace count
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //total:總金額,type:項目所花總費用,used:本次項目當下費用
+
+        static void UsedMoney(ref int total, ref int type, int used,int use)
+        {
+            // (new)total= (old)total-use
+            total -= use;
+            // (new)type= (old)type-use
+            type += use;
+            
+
+
+            //紀錄檔案資料
+        }
+        //
+
+        //COMBOBOX
+        public class cboDataList
+        {
+            public string cbo_Name { get; set; }
+            public string cbo_Value { get; set; }
+        }
+        //運算值變數
+        public static int TotalMoney = 0, eat = 0, cloth = 0, house = 0, traffic = 0, used,use=0;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //初始化設定
+            start();
+            
+            //在combobox1選單中加入選項
+            List<cboDataList> lis_DataList = new List<cboDataList>()
+            {
+                new cboDataList
+                {
+                    cbo_Name = "收入",
+                    cbo_Value = "1"
+                },
+                new cboDataList
+                {
+                    cbo_Name = "支出",
+                    cbo_Value = "2"
+                },
+                
+            };
+
+            comboBox1.DataSource = lis_DataList;
+            comboBox1.DisplayMember = "cbo_Name";
+            comboBox1.ValueMember = "cbo_Value";
+        }
+        private void start()
+        {
+
+            //取消 收入與支出欄位輸入功能
+            this.textBox1.Enabled = false;
+            this.textBox2.Enabled = false;
+
+
+
+
+
+
+        }
+
+
+        
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //假設選單項目是空的不出錯
+            if (comboBox1.SelectedValue == null)
+            {
+                return;
+            }
+            //選單變數
+            string buy_sell_value = comboBox1.SelectedValue.ToString();
+            //MessageBox.Show(buy_sell_value);
+            switch (buy_sell_value)
+            {
+                //項目1:收入
+                case "1":
+                    comboBox1.BackColor = Color.Red;
+                    check1();
+                    MessageBox.Show("開始記錄收入");
+                    //選單2加入項目,與項目2的內容會不同
+                    List<cboDataList> lis_DataList1 = new List<cboDataList>()
+            {
+
+                new cboDataList
+                {
+                    cbo_Name = "薪水",
+                    cbo_Value = "1"
+                },
+                new cboDataList
+                {
+                    cbo_Name = "獎金",
+                    cbo_Value = "2"
+                },
+
+            };
+
+                    comboBox2.DataSource = lis_DataList1;
+                    comboBox2.DisplayMember = "cbo_Name";
+                    comboBox2.ValueMember = "cbo_Value";
+                    break;
+                    //項目:支出
+                case "2":
+                    comboBox1.BackColor = Color.Blue;
+                    check2();
+                    MessageBox.Show("開始記錄支出");
+
+                    //選單2加入項目,與項目1的內容會不同
+                        List<cboDataList> lis_DataList = new List<cboDataList>()
+            {
+                        
+                new cboDataList
+                {
+                    cbo_Name = "飲食",
+                    cbo_Value = "1"
+                },
+                new cboDataList
+                {
+                    cbo_Name = "衣服",
+                    cbo_Value = "2"
+                },
+                new cboDataList
+                {
+                    cbo_Name = "住",
+                    cbo_Value = "3"
+                },
+                new cboDataList
+                {
+                    cbo_Name = "交通",
+                    cbo_Value = "4"
+                },
+
+            };
+
+                    comboBox2.DataSource = lis_DataList;
+                    comboBox2.DisplayMember = "cbo_Name";
+                    comboBox2.ValueMember = "cbo_Value";
+                    break;
+                default:
+                    comboBox1.BackColor = Color.White;
+                    break;
+            }
+        }
+        private void check1()
+        {
+            //取消支出欄位輸入功能
+            this.textBox1.Enabled = false;
+            this.textBox2.Enabled = true;
+
+
+
+
+
+
+
+
+
+
+        }
+        private void check2()
+        {
+            //取消收入欄位輸入功能
+            this.textBox1.Enabled = true;
+            this.textBox2.Enabled = false;
+
+
+
+
+
+
+
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //選單項目變更不出錯
+            if (comboBox2.SelectedValue == null)
+            {
+                return;
+            }
+   
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //判斷第二個選單變數
+            int select;
+            string a = comboBox2.SelectedIndex.ToString();
+            select = comboBox2.SelectedIndex;
+            //沒有資料,使用者按確認不出錯
+            try
+            {
+                int user = int.Parse(textBox2.Text.ToString());
+                int cash = int.Parse(label10.Text.ToString());
+                int money = int.Parse(label12.Text.ToString());
+                int costn = int.Parse(label17.Text.ToString());
+                int tcost = int.Parse(label11.Text.ToString());
+
+                //判斷第一個選單的選項是否為收入
+                if (comboBox1.SelectedIndex == 0)
+                {
+
+                    TotalMoney += user;
+                    //計算剩餘金額
+                    if (cash != 0)
+                    {
+                        cash = money - tcost;
+                    }
+                    //讓剩餘金額等於現金
+                    else
+                    {
+                        cash = user;
+                    }
+
+                    label12.Text = TotalMoney.ToString();
+                    label10.Text = cash.ToString();
+
+                }
+                //判斷第一個選單的選項是否為支出
+                else
+                {
+                    use = int.Parse(textBox1.Text.ToString());
+                    //  (new)used= (old)used+use
+                    //總花費等於本次花費
+                    used += use;
+                    try
+                    {
+                        //第二個選單項目設計
+
+                        //string buy_sell_value = comboBox2.SelectedValue.ToString();
+                        switch (select)
+                        {
+                            case 0:
+                                comboBox2.BackColor = Color.Yellow;
+                                check2();
+                                //MessageBox.Show("開始記錄飲食");
+
+                                break;
+                            case 1:
+                                comboBox2.BackColor = Color.Yellow;
+                                check2();
+                                //MessageBox.Show("開始記錄衣服");
+
+                                break;
+                            case 2:
+                                comboBox2.BackColor = Color.Yellow;
+                                check2();
+                                //MessageBox.Show("開始記錄房子");
+                                break;
+                            case 3:
+                                comboBox2.BackColor = Color.Yellow;
+                                check2();
+                                //MessageBox.Show("開始記錄交通");
+
+                                break;
+                            default:
+                                comboBox1.BackColor = Color.White;
+                                break;
+                        }
+                        //判斷第二個選單項目內容
+                        if (comboBox2.SelectedIndex == 0)
+                        {
+                            UsedMoney(ref TotalMoney, ref eat, used, use);
+                            label13.Text = eat.ToString();
+                            label10.Text = TotalMoney.ToString();
+                            label11.Text = used.ToString();
+                            label17.Text = use.ToString();
+                        }
+                        else if (comboBox2.SelectedIndex == 1)
+                        {
+                            UsedMoney(ref TotalMoney, ref cloth, used, use);
+                            label14.Text = cloth.ToString();
+                            label10.Text = TotalMoney.ToString();
+                            label11.Text = used.ToString();
+                            label17.Text = use.ToString();
+                        }
+                        else if (comboBox2.SelectedIndex == 2)
+                        {
+                            UsedMoney(ref TotalMoney, ref house, used, use);
+                            label15.Text = house.ToString();
+                            label10.Text = TotalMoney.ToString();
+                            label11.Text = used.ToString();
+                            label17.Text = use.ToString();
+                        }
+                        else if (comboBox2.SelectedIndex == 3)
+                        {
+                            UsedMoney(ref TotalMoney, ref traffic, used, use);
+                            label16.Text = traffic.ToString();
+                            label10.Text = TotalMoney.ToString();
+                            label11.Text = used.ToString();
+                            label17.Text = use.ToString();
+                        }
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show("請輸入支出內容");
+                        return;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("請輸入收入內容");
+                return;
+            }
+
+            
+
+
+        }
+    }
+}
